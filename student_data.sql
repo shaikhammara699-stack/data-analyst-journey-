b1b7699983b3977e9f1c7004b7d651a82f604f53
shaikhammara699-stack <shaikhammara699@gmail.com>
2026-03-21 13:17:43 +0530

    Create student_data.sql

---
+++ student_data.sql
@@ -0,0 +1,15 @@
+CREATE TABLE students (
+  id INTEGER,
+  name TEXT,
+  marks INTEGER
+);
+
+INSERT INTO students VALUES (1, 'Ammara', 80);
+INSERT INTO students VALUES (2, 'Rahul', 60);
+INSERT INTO students VALUES (3, 'Zara', 90);
+INSERT INTO students VALUES (4, 'Vicky', 120);
+INSERT INTO students VALUES (5, 'Wajid', 95);
+INSERT INTO students VALUES (6, 'Wasim', 84);
+INSERT INTO students VALUES (7, 'Arbina', 110);
+
+SELECT * FROM students;
